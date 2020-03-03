df471fdb47e371566702fd1b6e6dc96eb442d2dd
Chris Cyr <chris.dana.cyr@gmail.com>
2020-03-03 10:25:06 -0600

    checking for changes

---
+++ src/main/java/Login.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet(name="Login", urlPatterns = "/login")
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if (username.equals("admin") && password.equals("password")) {
+            response.sendRedirect("./profile");
+        } else {
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+    }
+}
