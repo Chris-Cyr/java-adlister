05c70650b5716abd3bf2206ed66827554c0c59f2
Chris Cyr <chris.dana.cyr@gmail.com>
2020-03-04 10:24:06 -0600

    updated servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -3,18 +3,23 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     int count = 0;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
-    count++;
         response.setContentType("text/html");
         String name = request.getParameter("name");
         if(name == null){
             name = "World";
         }
         System.out.println(name);
+        String reset = request.getParameter("reset");
+        if(reset != null){
+            count = 0;
+        }else{
+            count++;
+        }
         response.getWriter().println("<h1>Hello " + name + "!</h1>");
         response.getWriter().println("<p>Page visits = " + count + "</p>");
 
