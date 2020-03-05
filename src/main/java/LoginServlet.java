ba794b06a6809c62e47babf60faf67233b678131
Chris Cyr <chris.dana.cyr@gmail.com>
2020-03-05 09:28:56 -0600

    updating web-inf

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -8,15 +8,16 @@ import java.io.IOException;
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
+        boolean validLogin = username.equals("admin") && password.equals("password");
 
-        if (validAttempt) {
+        if (validLogin) {
+            request.getSession().setAttribute("validLogin",true);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
